c5a984620e940e242c455505a4fe9d6f43a72609
Marijn van Dongen <marijnvdongen@gmail.com>
2022-11-22 18:35:26 +0100

    Save instead of show

--- stitch.py
+++ stitch.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 # Opens all images in an image folder and appends it to a list
 image_folder = "test_images"
@@ -7,15 +8,15 @@ images = []
 for i in os.listdir(image_folder):
     image = cv2.imread(f'{image_folder}/{i}')
     images.append(image)
-
 print(len(images))
 
-stitcher = cv2.Stitcher.create()
+stitcher = cv2.Stitcher.create(mode=1)
 (status, result) = stitcher.stitch(images)
 if (status == cv2.STITCHER_OK):
     print("Stitch Generated")
 else:
     print("Stitch Failed")
 
-cv2.imshow("test", result)
+cv2.imwrite("result_image/stitch.jpg", result)
+
 cv2.waitKey(0)
